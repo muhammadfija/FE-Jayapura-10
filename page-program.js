6bea286b7d043daff4a030edc0ea032021fbd930
unknown <rahmazahrani102@gmail.com>
2023-09-19 06:02:34 +0700

    page program

---
+++ page-program.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let menu = document.querySelector ("#menu-icon");
+    let navbar = document.querySelector (".navbar-program");
+
+    if (menu && navbar){
+    menu.onclick = () => {
+        menu.classList.toggle("bx-x");
+        navbar.classList.toggle("active");
+    };
+}
+});
