b1072f9ef5bb5a97eaa6bd4b82e537e159c8d129
OchaMeilyla <ochameilyla@gmail.com>
2023-09-29 18:29:20 +0700

    Edited show pop-up

--- Program/page-program.js
+++ Program/page-program.js
@@ -10,14 +10,33 @@ document.addEventListener("DOMContentLoaded", function(){
 }
 });
 
-var popup = document.getElementById("popup-show");
-var tutup = document.getElementsByClassName("close-btn");
-var btn = document.getElementById("myButton");
+// var popup = document.getElementById("popup-show");
+// var tutup = document.getElementsByClassName("close-btn");
+// var btn = document.getElementByClassName("btn-content");
 
-btn.onclick = function(){
-    popup.style.display = "block !important";
-};
+// btn.onclick = function(){
+//     popup.style.display = "block !important";
+// };
 
-tutup.onclick = function(){
-    popup.style.display = "none";
-};
+// tutup.onclick = function(){
+//     popup.style.display = "none";
+// };
+
+const buttonOpenPopup = document.querySelector("#tombol-muncul-popup");
+const popup = document.querySelector("#popup-show");
+
+buttonOpenPopup.addEventListener("click",function() {
+    popup.style.display = "block";
+});
+
+const buttonClosePopup = document.querySelector(".close-btn");
+
+buttonClosePopup.addEventListener("click", function () {
+    popup.style.display = "none"; // Menutup pop-up
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target === popup) {
+        popup.style.display = "none"; // Menutup pop-up
+    }
+});
